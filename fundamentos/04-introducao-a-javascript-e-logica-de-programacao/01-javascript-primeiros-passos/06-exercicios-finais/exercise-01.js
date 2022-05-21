5cf32be0e3d5ddad14495c73877ad56f0d853a89
Luiz Pereira <lfhbpereira@gmail.com>
2022-05-21 20:56:18 -0300

    Add exercise done

---
+++ fundamentos/04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/06-exercicios-finais/exercise-01.js
@@ -0,0 +1,17 @@
+const a = 4;
+const b = 2;
+
+let addition = a + b;
+console.log("Adição: " + addition);
+
+let subtraction = a - b;
+console.log("Subtração: " + subtraction);
+
+let multiplication = a * b;
+console.log("Multiplicação: " + multiplication);
+
+let division = a / b;
+console.log("Divisão: " + division);
+
+let remainder = a % b;
+console.log("Módulo: " + remainder);
