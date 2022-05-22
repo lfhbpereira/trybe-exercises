3f568336539ea66e28181d4cf21d3ec7ac784ec8
Luiz Pereira <lfhbpereira@gmail.com>
2022-05-21 23:07:03 -0300

    Update quotation marks

--- fundamentos/04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/04-operadores-logicos/scripts.js
+++ fundamentos/04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/04-operadores-logicos/scripts.js
@@ -2,30 +2,30 @@
 
 const currentHour = 7;
 
-let message = "";
+let message = '';
 
 if (currentHour >= 22) {
-  console.log(message = "Não deveríamos comer nada, é hora de dormir");
+  console.log(message = 'Não deveríamos comer nada, é hora de dormir');
 } else if (currentHour < 22 && currentHour >= 18) {
-  console.log(message = "Rango da noite, vamos jantar :D");
+  console.log(message = 'Rango da noite, vamos jantar :D');
 } else if (currentHour < 18 && currentHour >= 14) {
-  console.log(message = "Vamos fazer um bolo pro café da tarde?");
+  console.log(message = 'Vamos fazer um bolo pro café da tarde?');
 } else if (currentHour < 14 && currentHour >= 11) {
-  console.log(message = "Hora do almoço!!!");
+  console.log(message = 'Hora do almoço!!!');
 } else if (currentHour < 11 && currentHour >= 4) {
-  console.log(message = "Hmmm, cheiro de café recém-passado");
+  console.log(message = 'Hmmm, cheiro de café recém-passado');
 }
 
 console.log(message);
 
 // Logical OR (||)
 
-let weekDay = "quarta-feira";
+let weekDay = 'quarta-feira';
 
-if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
-  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+  console.log('Oba, mais um dia de aprendizado na Trybe >:D');
 } else {
-  console.log("FINALMENTE, descanso merecido UwU");
+  console.log('FINALMENTE, descanso merecido UwU');
 }
 
 // Logical NOT (!)
