e5150a0c00b2f5428e78ce12db687e5b3e1b9f19
Luiz Pereira <lfhbpereira@gmail.com>
2022-05-22 11:51:18 -0300

    Update quotation marks and Rename file

---
+++ fundamentos/04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/06-exercicios-finais/scripts.js
@@ -0,0 +1,19 @@
+// 01
+
+const a = 4;
+const b = 2;
+
+let addition = a + b;
+console.log('Adição: ' + addition);
+
+let subtraction = a - b;
+console.log('Subtração: ' + subtraction);
+
+let multiplication = a * b;
+console.log('Multiplicação: ' + multiplication);
+
+let division = a / b;
+console.log('Divisão: ' + division);
+
+let remainder = a % b;
+console.log('Módulo: ' + remainder);
