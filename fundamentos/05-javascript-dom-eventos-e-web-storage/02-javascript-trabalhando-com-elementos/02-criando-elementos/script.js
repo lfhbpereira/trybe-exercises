10892122273976ea3355ec9f125588fcefd13642
Luiz Pereira <lfhbpereira@gmail.com>
2022-06-16 18:48:25 -0300

    Add exercise 2 done

--- fundamentos/05-javascript-dom-eventos-e-web-storage/02-javascript-trabalhando-com-elementos/02-criando-elementos/script.js
+++ fundamentos/05-javascript-dom-eventos-e-web-storage/02-javascript-trabalhando-com-elementos/02-criando-elementos/script.js
@@ -1,2 +1,5 @@
 const irmaoDoElementoOndeVoceEsta = document.createElement('section');
 document.getElementById('elementoOndeVoceEsta').parentElement.appendChild(irmaoDoElementoOndeVoceEsta);
+
+const filhoDoElementoOndeVoceEsta = document.createElement('section');
+document.getElementById('elementoOndeVoceEsta').appendChild(filhoDoElementoOndeVoceEsta);
