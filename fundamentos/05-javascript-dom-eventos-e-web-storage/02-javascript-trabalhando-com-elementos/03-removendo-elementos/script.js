f64a4e84be0e53916e7f61d7cb3ec3acf855a12b
Luiz Pereira <lfhbpereira@gmail.com>
2022-06-16 21:38:59 -0300

    Add exercise done

---
+++ fundamentos/05-javascript-dom-eventos-e-web-storage/02-javascript-trabalhando-com-elementos/03-removendo-elementos/script.js
@@ -0,0 +1,7 @@
+const pai = document.getElementById('pai');
+pai.firstElementChild.remove();
+
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+elementoOndeVoceEsta.lastElementChild.remove();
+elementoOndeVoceEsta.nextElementSibling.remove();
+elementoOndeVoceEsta.nextElementSibling.remove();
