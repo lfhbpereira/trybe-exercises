08666d2fad33b65ba218314d1b82950f4f2ffe0f
Luiz Pereira <lfhbpereira@gmail.com>
2022-06-24 00:17:24 -0300

    Add exercise 1 done

---
+++ fundamentos/08-higher-order-functions-do-javascript-es6/02-javascript-es6-foreach-find-some-every-sort/03-some-every/exercises.js
@@ -0,0 +1,9 @@
+// 01
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some((element) => element === name);
+};
+
+console.log(hasName(names, 'Ana'));
