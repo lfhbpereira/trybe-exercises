e8ccd6939397f53a22886353c833dc6ba3ffed7c
Luiz Pereira <lfhbpereira@gmail.com>
2022-06-24 00:27:23 -0300

    Add exercise 2 done

--- fundamentos/08-higher-order-functions-do-javascript-es6/02-javascript-es6-foreach-find-some-every-sort/03-some-every/exercises.js
+++ fundamentos/08-higher-order-functions-do-javascript-es6/02-javascript-es6-foreach-find-some-every-sort/03-some-every/exercises.js
@@ -7,3 +7,19 @@ const hasName = (arr, name) => {
 };
 
 console.log(hasName(names, 'Ana'));
+
+// 02
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((person) => person.age === minimumAge);
+};
+
+console.log(verifyAges(people, 18));
