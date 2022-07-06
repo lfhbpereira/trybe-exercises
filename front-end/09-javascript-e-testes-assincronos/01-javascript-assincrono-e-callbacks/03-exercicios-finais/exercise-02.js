bb65d3ecd2372f63ccb6a9a34ecadd82467ed2a3
Luiz Pereira <lfhbpereira@gmail.com>
2022-07-06 19:55:44 -0300

    Add exercise 2 done

--- front-end/09-javascript-e-testes-assincronos/01-javascript-assincrono-e-callbacks/03-exercicios-finais/exercise-02.js
+++ front-end/09-javascript-e-testes-assincronos/01-javascript-assincrono-e-callbacks/03-exercicios-finais/exercise-02.js
@@ -28,3 +28,5 @@ const jupiter = {
 console.log(planetDistanceFromSun(mars)); // A
 setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
 setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+// Resposta: A ordem de finalização de execução é A, C, B.
