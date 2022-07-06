6a03652db6cd86909b181440f151f1f36499b96d
Luiz Pereira <lfhbpereira@gmail.com>
2022-07-06 13:19:40 -0300

    Add JS

---
+++ front-end/09-javascript-e-testes-assincronos/01-javascript-assincrono-e-callbacks/01-callbacks/exercise-01.js
@@ -0,0 +1,13 @@
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = () => {
+  const userToReturn = {
+    firstName: 'Ivan',
+    lastName: 'Ivanovich',
+    nationality: 'Russian'
+  };
+};
+
+console.log(getUser());
+console.log(getUser());
