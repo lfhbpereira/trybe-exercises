19f63a7c4ffd64407d38d6287125f144d242a39f
Luiz Pereira <lfhbpereira@gmail.com>
2022-07-06 19:43:02 -0300

    Add JS

---
+++ front-end/09-javascript-e-testes-assincronos/01-javascript-assincrono-e-callbacks/03-exercicios-finais/exercise-06.js
@@ -0,0 +1,21 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+}
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+sendMarsTemperature(greet, handleError);
