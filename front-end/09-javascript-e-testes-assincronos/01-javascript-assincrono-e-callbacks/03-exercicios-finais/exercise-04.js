23f120cbaee349260254c2f4f8d47feaa8d4cb53
Luiz Pereira <lfhbpereira@gmail.com>
2022-07-06 19:37:55 -0300

    Add JS

---
+++ front-end/09-javascript-e-testes-assincronos/01-javascript-assincrono-e-callbacks/03-exercicios-finais/exercise-04.js
@@ -0,0 +1,8 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+sendMarsTemperature();
