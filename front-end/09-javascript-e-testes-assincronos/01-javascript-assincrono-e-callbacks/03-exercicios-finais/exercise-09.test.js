fabe1b7f12be7121c5850208f6fba1fe62c55276
Luiz Pereira <lfhbpereira@gmail.com>
2022-07-12 21:30:27 -0300

    Add exercise 9 done

--- front-end/09-javascript-e-testes-assincronos/01-javascript-assincrono-e-callbacks/03-exercicios-finais/exercise-09.test.js
+++ front-end/09-javascript-e-testes-assincronos/01-javascript-assincrono-e-callbacks/03-exercicios-finais/exercise-09.test.js
@@ -1,9 +1,22 @@
 const { getPokemonDetails } = require('./exercise-08');
 
 describe('A função getPokemonDetails', () => {
-  it('retorna erro quando procuramos um pokemon que não existe no banco de dados', () => {
+  it('retorna erro quando procuramos um pokemon que não existe no banco de dados', (done) => {
+    const err = new Error('Não temos esse pokémon para você :(');
+    function callback(error, result) {
+      expect(error).toEqual(err);
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Pikachu', callback);
   });
 
-  it('retorna um pokemon que existe no banco de dados', () => {
+  it('retorna um pokemon que existe no banco de dados', (done) => {
+    function callback(error, result) {
+      expect(result).toBe('Olá, seu pokémon é o Squirtle, o tipo dele é Water e a habilidade principal dele é Water Gun');
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Squirtle', callback);
   });
 });
