9053b712d2c2f2eadf1b2fd4b44b7d28926e9aff
Luiz Pereira <lfhbpereira@gmail.com>
2022-07-14 23:42:00 -0300

    Include parentheses in the parameter

--- fundamentos/08-higher-order-functions-do-javascript-es6/02-javascript-es6-foreach-find-some-every-sort/05-exercicios-finais/exercise-01.js
+++ fundamentos/08-higher-order-functions-do-javascript-es6/02-javascript-es6-foreach-find-some-every-sort/05-exercicios-finais/exercise-01.js
@@ -62,7 +62,7 @@ const books = [
 ];
 
 function authorBornIn1947() {
-  return books.find(book => book.author.birthYear === 1947).author.name;
+  return books.find((book) => book.author.birthYear === 1947).author.name;
 }
 
 console.log(authorBornIn1947());
