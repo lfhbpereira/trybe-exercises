fac3ac8dc9a58e46dbc4db08b6df907171f4fe16
Luiz Pereira <lfhbpereira@gmail.com>
2022-07-27 20:57:31 -0300

    Add exercise 1 done and Rename file

---
+++ front-end/10-introducao-a-react/02-componentes-react/01-fixation-exercises-10-2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from './Image';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
+      </div>
+    );
+  }
+}
+
+export default App;
